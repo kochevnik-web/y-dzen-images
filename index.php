320121062426a41e3d50a4fd8acdcc6192cf13db
kochevnik-web <kacevnik@yandex.ru>
2020-12-06 14:36:38 +0300

    Init Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Yandex Dzen Images
+ * Plugin URI:  https://wordpress.org
+ * Version:     1.0.0
+ * Description: The plugin creates images as in Yandex Dzen when uploading images to the site
+ * Author:      Dmitriy Kovalev
+ * Author URI:  https://github.com/kochevnik-web
+ *
+ */
